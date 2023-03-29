848c9835b8829f6d50dce9b60bc9c98c44c46916
martenji01 <martacrd01@gmail.com>
2023-03-29 08:16:06 +0200

    feat(users): basic users

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,6 +4,8 @@ import { DishModule } from './dish/dish.module';
 import { MongooseModule } from '@nestjs/mongoose';
 import { ConfigModule } from '@nestjs/config';
 import { MongodbConfigService } from './config/mongoDb.config';
+import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
   imports: [
@@ -17,7 +19,9 @@ import { MongodbConfigService } from './config/mongoDb.config';
     }),
     //MongooseModule.forRoot(process.env.DB_CONNECTION),
     OrderModule,
-    DishModule]
+    DishModule,
+    AuthModule,
+    UsersModule]
 })
 
 
